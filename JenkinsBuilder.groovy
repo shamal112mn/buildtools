8b5944434da0c00c60836e50394d07128a55ac50
Azat <you@example.com>
2021-04-01 22:38:44 +0000

    username updated

--- JenkinsBuilder.groovy
+++ JenkinsBuilder.groovy
@@ -3,7 +3,7 @@
   def gitCommitHash = ""
   def dockerImage = ""
   def repositoryName = "${JOB_NAME}"
-  def registry = "fuchicorp/${repositoryName}"
+  def registry = "shamal112mn/${repositoryName}"
   def registryCredentials = 'docker-hub-creds'
 
   def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '')
